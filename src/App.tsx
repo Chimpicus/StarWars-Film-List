1bbceb1d3da0f65bbabe64e4bf3b88122a752362
Casey Boyes <casey.boyes@fortiumtech.com>
2022-03-05 10:15:50 +0000

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import FilmList from './components/FilmList';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log('test');
+
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <Navbar />
+                <div className="conent">
+                    <Routes>
+                        <Route path="/" element={<FilmList />}></Route>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/components/Login.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Login = () => {
+  return <h1 className="login">Log in</h1>;
+};
+
+export default Login;
